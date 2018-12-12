1b8f8072b2f7903fcbd09fc873e72d9d359aa56e
Your Name <you@example.com>
2018-12-12 20:47:22 +0400

    test Lesson_4 untill page 16

---
+++ script.js
@@ -0,0 +1,32 @@
+// var clickCount = 0;
+// function clickHandler(evt){
+//    clickCount++;
+//    console.log(evt);
+//    var str = "Thanks for clicking " + clickCount;
+//    this.innerText = str;
+// }
+
+// var p = document.getElementById("pElement");
+// p.addEventListener("click", clickHandler);
+
+//____________________________________________
+
+
+// function bodyClick(evt){
+//     console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+//  }
+//  window.onclick = bodyClick;
+
+//_________________________________________________
+// function click(evt) {
+//     console.log(evt.pageX, evt.pageY);
+//  }
+//  window.onclick = click;
+
+//_________________________________________
+
+// function keydown(evt) {
+//     console.log("You printed " + evt.key);
+//  }
+//  window.onkeydown = keydown;
+
